17244d2f4a9fc476f875c3c0b68e08c12d646462
smithbeasley <smithbeasley@gmail.com>
2021-02-11 03:10:11 -0600

    todo updates

--- vite-todo/src/App.vue
+++ vite-todo/src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <h1 class="text-2xl font-bold">Hello There</h1>
+<h1 class="text-2xl font-bold">My To Do App</h1>
+<input type="text" placeholder="New Todo"/>
+<h3>Pending Items</h3>
+<ul>
+  <li>One</li>
+</ul>
+<h3>Completed Items</h3>
+<ul>
+  <li>Two</li>
+</ul>
 </template>
 
-<script setup>
-</script>
 
-<style>
+<script setup></script>
 
-</style>
